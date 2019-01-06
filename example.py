b13cd41a4328ccc9e7f16c1b20da98da6da033d7
randysuen1991 <randysuen1991@gmail.com>
2019-01-06 20:41:08 +0800

    new

--- example.py
+++ example.py
@@ -132,6 +132,8 @@ def example4():
     output = tf.nn.batch_normalization(input, batch_mean, batch_var, 3, 2, 0)
     print(sess.run(output))
 
+def ex():
+    pass
 
 if __name__ == '__main__':
     example3()
